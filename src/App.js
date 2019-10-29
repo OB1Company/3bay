04f2fc6fbbe50c85ec7330d1a1e6ba2eef090910
Rachel Black <rachblondon@gmail.com>
2019-10-29 17:23:49 +0100

    boilerplat to get 3Box profile with metamask

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import Box from "3box";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getThreeBox = async (address) => {
+  const profile = await Box.getProfile(address);
+  console.log(profile);
+  return profile;
+};
+
+export default class App extends Component {
+
+  state = {
+    needToAWeb3Browser : false,
+  }
+  async getAddressFromMetaMask() {
+    if (typeof window.ethereum == "undefined") {
+      this.setState({ needToAWeb3Browser: true });
+    } else {
+      window.ethereum.autoRefreshOnNetworkChange = false; //silences warning about no autofresh on network change
+      const accounts = await window.ethereum.enable();
+      this.setState({ accounts });
+    }
+  }
+  async componentDidMount() {
+    await this.getAddressFromMetaMask();
+    if (this.state.accounts) {
+      const threeBox = await getThreeBox(this.state.accounts[0]);
+      this.setState({ threeBox });
+    }
+  }
+  render() {
+
+    if(this.state.needToAWeb3Browser){
+      return <h1>Please install metamask</h1>
+    }
+
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/photos">Photos</Link>
+              </li>
+              <li>
+                <Link to="/messager">Messenger</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/messenger">
+              <Messenger />
+            </Route>
+            <Route path="/photos">
+              <Photos />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+class Profile extends Component {
+  render() {
+    return <h2>Profile </h2>;
+  }
+}
+
+class Photos extends Component {
+  render() {
+    return <h2>Photos</h2>;
+  }
+}
+
+function Messenger() {
+  return <h2>Messenger</h2>;
+}
