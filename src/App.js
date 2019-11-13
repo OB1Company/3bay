60b70485c982b35f739c7d63cc681ac6e43aa5e6
Rachel Black <rachblondon@gmail.com>
2019-11-13 11:33:42 +0100

    moving home

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { BounceLoader } from "react-spinners";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppForm from "./components/AppForm";
 import Nav from './components/Nav'; 
-import ProfileHover from 'profile-hover';
+
+import Home from './pages/Home';
 
 const getThreeBox = async address => {
   const profile = await Box.getProfile(address);
@@ -91,51 +92,7 @@ export default class App extends Component {
   }
 }
 
-class Home extends Component {
 
-  render() {
-
-    return (
-      <div className="container" style={{ textAlign: "center" }}>
-        <h1>Home</h1>
-        {/* TODO fix bootstrap grid */}
-        <div className="row">
-          {!this.props.posts && (
-            <div style={{ width: "100px", margin: "auto" }}>
-              <BounceLoader color={"blue"} />
-            </div>
-          )}
-          {this.props.posts &&
-            this.props.posts.map((post, i) => (
-              <div key={i}>
-                <div className="col">
-                  <h5>{post.message.name ? post.message.name : "unknown"}</h5>
-                  <img
-                    style={{ width: "200px" }}
-                    src={
-                      post.message.appImage
-                        ? post.message.appImage
-                        : "https://via.placeholder.com/200"
-                    }
-                  />
-                  <p>{post.message.description}</p>
-                  {post.message.url && (
-                    <p>
-                      <a href={post.message.url} target="_blank">
-                        website
-                      </a>
-                    </p>
-                  )}
-                  <ProfileHover address={post.author} />
-                </div>
-                {i % 3 == 0 && i != 0 && <div className="w-100"></div>}
-              </div>
-            ))}
-        </div>
-      </div>
-    );
-  }
-}
 
 class Profile extends Component {
   render() {
