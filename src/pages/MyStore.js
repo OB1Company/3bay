d63a89716b0b31e8380075bfce6a50ce4942af11
Dr Washington Sanchez <washington.sanchez@gmail.com>
2020-07-29 21:02:10 +1000

    tweak: Style changes (part 2)

--- src/pages/MyStore.js
+++ src/pages/MyStore.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
+import { CardColumns, Card } from "react-bootstrap";
 import { BounceLoader } from "react-spinners";
 
 const styles = {
+  column: {
+    width: "100%",
+  },
   background: {
     textAlign: "center",
   },
   wrapper: {
     padding: "20px",
+    background: "rgb(0,0,0,0)",
+    borderWidth: "0",
   },
   cardWrapper: {
     width: "100%",
     borderRadius: "20px",
     alignContent: "center",
-    background: "#ededed",
-    boxShadow: "-40px 40px 80px #d1d1d1, 40px -40px 80px #ffffff",
+    background: "#ffffff",
+    boxShadow: "-20px 20px 40px #e0e0e0, 20px -20px 40px #ffffff",
   },
   image: {
     width: "100%",
@@ -62,55 +68,51 @@ class ListingCard extends Component {
   render() {
     return (
       <>
-        <div className="col-sm-4">
-          <div style={styles.wrapper}>
-            <div style={styles.cardWrapper}>
-              <img
-                alt="Listing"
-                style={styles.image}
-                src={
-                  this.props.post.message.listingImage
-                    ? this.props.post.message.listingImage
-                    : "https://via.placeholder.com/200"
-                }
-                onError={(ev) =>
-                  (ev.target.src =
-                    "https://stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png")
-                }
-              />
-              <div style={styles.copyWrapper}>
-                <p style={styles.name}>
-                  {this.props.post.message.name
-                    ? this.props.post.message.name
-                    : "Unnamed"}
-                </p>
-                <p style={styles.price}>
-                  {this.props.post.message.price
-                    ? this.props.post.message.price
-                    : "$0"}
-                </p>
+        <Card style={styles.wrapper}>
+          <div style={styles.cardWrapper}>
+            <img
+              alt="Listing"
+              style={styles.image}
+              src={
+                this.props.post.message.listingImage
+                  ? this.props.post.message.listingImage
+                  : "https://via.placeholder.com/200"
+              }
+              onError={(ev) =>
+                (ev.target.src =
+                  "https://stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder-350x350.png")
+              }
+            />
+            <div style={styles.copyWrapper}>
+              <p style={styles.name}>
+                {this.props.post.message.name
+                  ? this.props.post.message.name
+                  : "Unnamed"}
+              </p>
+              <p style={styles.price}>
+                {this.props.post.message.price
+                  ? this.props.post.message.price
+                  : "$0"}
+              </p>
 
-                <p style={styles.description}>
-                  {this.props.post.message.description}
-                </p>
+              <p style={styles.description}>
+                {this.props.post.message.description}
+              </p>
 
-                <p style={styles.shippingAddress}>
-                  {this.props.post.message.needsAddress === true
-                    ? "📦"
-                    : " "}
-                </p>
+              <p style={styles.shippingAddress}>
+                {this.props.post.message.needsAddress === true ? "📦" : " "}
+              </p>
 
-                {/* <Modal
+              {/* <Modal
                   app={this.props.post.message}
                   threeBox={this.props.threeBox}
                   space={this.props.space}
                   box={this.props.box}
                   usersAddress={this.props.usersAddress}
                 /> */}
-              </div>
             </div>
           </div>
-        </div>
+        </Card>
         {(this.props.i + 1) % 3 === 0 && <div className="w-100"></div>}
       </>
     );
@@ -131,8 +133,7 @@ export default class Home extends Component {
               <BounceLoader color={"blue"} />
             </div>
           )}
-          <div className="container">
-            <div className="row">
+          <CardColumns style={styles.column}>
               {this.props.posts &&
                 this.props.posts.map((post, i) => {
                   return (
@@ -147,8 +148,7 @@ export default class Home extends Component {
                     />
                   );
                 })}
-            </div>
-          </div>
+          </CardColumns>
         </div>
       </div>
     );
